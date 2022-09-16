282a4a335b71be101641214e94255b75d122f636
PN-LT03\Student.admin <marygracevellina@gmail.com>
2022-09-16 14:58:58 +0800

    fuentes

--- sample.java
+++ sample.java
@@ -6,5 +6,11 @@ public class Main {
         car2 = new Car("DEF222", "Saab 9-5");
         car1.setSpeed(100.0);
         System.out.println("The speed of car1 is " + car1.getSpeed());
+   Car car1;
+        Car car2;
+        car1 = new Car("ABC111", "Volvo V70");
+        car2 = new Car("DEF222", "Saab 9-5");
+        car1.setSpeed(100.0);
+        System.out.println("The speed of car1 is " + car1.getSpeed());
     }
 }
